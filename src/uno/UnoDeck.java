4678d1d7f67c7cb19e81659bc3cf8edcbeaa40c7
Kirill Troisky <Kira12yudin@Yandex.ru>
2023-03-31 00:12:51 +0300

    second commit

--- src/uno/UnoDeck.java
+++ src/uno/UnoDeck.java
@@ -1,5 +1,9 @@
 package uno;
 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class UnoDeck {
     private UnoCard[] cards;
     private int cardsInDeck;
@@ -24,11 +28,59 @@ public class UnoDeck {
             cards[cardsInDeck++] = new UnoCard(color, value);
             cards[cardsInDeck++] = new UnoCard(color, value);
         }
-        UnoCard.Value[] values = new UnoCard.Value[]{UnoCard.Value.Wild, UnoCard.Value.Wild_Four};
-        for(UnoCard.Value value : values){
-            for(int i = 0; i < 4; i++){
+        UnoCard.Value[] values1 = new UnoCard.Value[]{UnoCard.Value.Wild, UnoCard.Value.Wild_Four};
+        for (UnoCard.Value value : values) {
+            for (int i = 0; i < 4; i++) {
                 cards[cardsInDeck++] = new UnoCard(UnoCard.Color.Wild, value);
             }
         }
+    }
+
+    public void replaceDeckWith(ArrayList<UnoCard> cards) {
+        this.cards = cards.toArray(new UnoCard[cards.size()]);
+        this.cardsInDeck = this.cards.length;
+    }
+
+    public boolean isEmpty() {
+        return cardsInDeck == 0;
+    }
+
+    public void shuffle() {
+        int n = cards.length;
+        Random random = new Random();
+        for (int i = 0; i < cards.length; i++) {
+            int randomValue = i + random.nextInt(n - i);
+            UnoCard randomCard = cards[randomValue];
+            cards[randomValue] = cards[i];
+            cards[i] = randomCard;
+        }
+    }
+
+    public UnoCard drawCard() throws IllegalArgumentException {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Не удается вытянуть карту, так как в колоде нет карт");
+        }
+        return cards[--cardsInDeck];
+    }
+
+    public ImageIcon drawCardImage() throws IllegalArgumentException {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Не удается взять карту, так как колода пуста");
+        }
+        return new ImageIcon(cards[--cardsInDeck].toString() + ",png");
+    }
+    public UnoCard[] drawCard(int n){
+        if (n < 0){
+            throw new IllegalArgumentException("Должен тянуть положительные карты, но пытался тянуть"+ n + "карты.");
+        }
+        if (n > cardsInDeck) {
+            throw new IllegalArgumentException("Не могу рисовать" + n + "карты, так как есть только" + cardsInDeck + "карты.");
+        }
+
+        UnoCard[] ret = new UnoCard[n];
+        for(int i = 0; i < n; i++){
+            ret[i] = cards[--cardsInDeck];
+        }
+        return ret;
     }
 }
